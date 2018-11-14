5561b5330b2ac0e21bb3b89eee1b54cca09e505b
woozico0521 <zyy960521.com>
2018-11-14 23:53:30 +0800

    增加vue-swipe插件

--- src/components/find/Banner.vue
+++ src/components/find/Banner.vue
@@ -1,62 +1,72 @@
 <template>
     <div>
-        <div id="container">
-            <ul id="imgs">
-                <li v-for="(item,index) in imgs" :key="index" v-show="index==nowIndex">
-                    <img :src="item" alt="">
-                </li>
-            </ul>
-            <ul id="tabs">
-                <li v-for="(item,index) in imgs.length" :key="index" :class="{selected:index==nowIndex}" @click="changeImg(index)">
-                    <small></small>
-                </li>
-            </ul>
-        </div>
+        <swipe class="my-swipe">
+            <swipe-item class="slide1"></swipe-item>
+            <swipe-item class="slide2"></swipe-item>
+            <swipe-item class="slide3"></swipe-item>
+            <swipe-item class="slide4"></swipe-item>
+            <swipe-item class="slide5"></swipe-item>
+            <swipe-item class="slide6"></swipe-item>
+            <swipe-item class="slide7"></swipe-item>
+        </swipe>    
+        
     </div>
 </template>
 
 <script>
+import {Swipe,SwipeItem} from 'vue-swipe'
+import 'vue-swipe/dist/vue-swipe.css'
 export default {
-    data(){
-        return{
-            imgs:[
-                require('@/assets/images/1541149369308.jpg'),
-                require('@/assets/images/1541476544886.jpg'),
-                require('@/assets/images/1541734306453.jpg'),
-                require('@/assets/images/1541644166239.jpg'),
-                require('@/assets/images/1541735286731.jpg'),
-                require('@/assets/images/1542088813434.jpg'),
-                require('@/assets/images/1542000824490.jpg'),
-            ],
-            nowIndex:0,
-            timer:null,
-        }
-    },
-    methods:{
-        play(){
-            this.timer=setInterval(()=>{
-                this.nowIndex++;
-                if(this.nowIndex==this.imgs.length){
-                    this.nowIndex=0;
-                }
-            },1000);
-        },
-        changeImg(index){
-            this.nowIndex=index;
-        }
-    },
-    mounted(){
-        this.play()
+    components:{
+        'swipe':Swipe,
+        'swipe-item':SwipeItem,
     }
 }
 </script>
 
 <style scoped>
-#container{width: 94%;height: 2.24rem;margin:0 auto; position: relative;border-radius: 0.1rem;overflow: hidden;}
-#imgs li{width: 100%;height: 100%;position: absolute;top:0;left: 0;}
-#container img{width: 100%;}
-#tabs{position:absolute;left:40%;bottom:15px;color: #fff;cursor: pointer;}
-#tabs li{width: 0.1rem;height: 0.1rem;float: left;line-height: 0.1rem;margin-right: 0.2rem}
-#tabs li small{width:100%;height:100%;display:inline-block;border-radius: 5px;background: rgba(255,255,255,0.7);}
-#tabs .selected small{background: rgba(255,255,255,1);}
+.my-swipe {
+  height: 118px;
+  color: #fff;
+  font-size: 10px;
+  text-align: center;
+}
+
+.slide1 {
+  background-image: url('/static/img/1541149369308.jpg');
+  color: #fff;
+  background-size: 100%;
+}
+
+.slide2 {
+  background-image: url('/static/img/1541476544886.jpg');
+  color: #000;
+  background-size: 100%;
+}
+ 
+.slide3 {
+  background-image: url('/static/img/1541644166239.jpg');
+  color: #fff;
+  background-size: 100%;
+}
+.slide4 {
+  background-image: url('/static/img/1541734306453.jpg');
+  color: #fff;
+  background-size: 100%;
+}
+.slide5 {
+  background-image: url('/static/img/1541735286731.jpg');
+  color: #fff;
+  background-size: 100%;
+}
+.slide6 {
+  background-image: url('/static/img/1542000824490.jpg');
+  color: #fff;
+  background-size: 100%;
+}
+.slide7 {
+  background-image: url('/static/img/1542088813434.jpg');
+  color: #fff;
+  background-size: 100%;
+}
 </style>
---
+++ src/components/find/Banner2.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div id="container">
+            <ul id="imgs">
+                <li v-for="(item,index) in imgs" :key="index" v-show="index==nowIndex">
+                    <img :src="item" alt="">
+                </li>
+            </ul>
+            <ul id="tabs">
+                <li v-for="(item,index) in imgs.length" :key="index" :class="{selected:index==nowIndex}" @click="changeImg(index)">
+                    <small></small>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            imgs:[
+                require('@/assets/images/1541149369308.jpg'),
+                require('@/assets/images/1541476544886.jpg'),
+                require('@/assets/images/1541734306453.jpg'),
+                require('@/assets/images/1541644166239.jpg'),
+                require('@/assets/images/1541735286731.jpg'),
+                require('@/assets/images/1542088813434.jpg'),
+                require('@/assets/images/1542000824490.jpg'),
+            ],
+            nowIndex:0,
+            timer:null,
+        }
+    },
+    methods:{
+        play(){
+            this.timer=setInterval(()=>{
+                this.nowIndex++;
+                if(this.nowIndex==this.imgs.length){
+                    this.nowIndex=0;
+                }
+            },1000);
+        },
+        changeImg(index){
+            this.nowIndex=index;
+        }
+    },
+    mounted(){
+        this.play()
+    }
+}
+</script>
+
+<style scoped>
+#container{width: 94%;height: 2.24rem;margin:0 auto; position: relative;border-radius: 0.1rem;overflow: hidden;}
+#imgs li{width: 100%;height: 100%;position: absolute;top:0;left: 0;}
+#container img{width: 100%;}
+#tabs{position:absolute;left:40%;bottom:15px;color: #fff;cursor: pointer;}
+#tabs li{width: 0.1rem;height: 0.1rem;float: left;line-height: 0.1rem;margin-right: 0.2rem}
+#tabs li small{width:100%;height:100%;display:inline-block;border-radius: 5px;background: rgba(255,255,255,0.7);}
+#tabs .selected small{background: rgba(255,255,255,1);}
+</style>
